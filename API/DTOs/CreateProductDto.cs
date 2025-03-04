924571a02939db6e439ea70c43fb473e4a8fc5c4
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-03-05 03:13:39 +1100

    Error handling and cors

---
+++ API/DTOs/CreateProductDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class CreateProductDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Range(minimum:0.01, maximum: double.MaxValue, ErrorMessage = "Price must be greater than 0.01")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string PictureUrl { get; set; } = string.Empty;
+
+        [Required]
+        public string Type { get; set; } = string.Empty;
+
+        [Required]
+        public string Brand { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity must be atleast 1")]
+        public int QuantityInStock { get; set; }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -23,11 +24,24 @@ builder.Services.AddDbContext<StoreContext>(options =>
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+// CORS support
+// CORS configuration below
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Exception handling in middleware level
+app.UseMiddleware<ExceptionMiddleware>();
+
+// CORS configuration
+// The API methods can only be accessed from below URL clients
+// Other clients cannot access it
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
