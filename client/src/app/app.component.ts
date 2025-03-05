e17db34c08e59f4018dc514de848ec76b19195fc
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-03-06 04:06:32 +1100

    services and API calls from Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { Product } from './shared/models/Product';
+import { ShopService } from './core/services/shop.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,21 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit
+{
+ 
   title = 'Skinet';
+  products: Product[] = [];
+  private shopService = inject(ShopService);
+
+  ngOnInit(): void 
+  {
+    this.shopService.getProducts().subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error(error),
+      //complete: () => console.log('Complete')
+    });
+  }
+
+
 }
