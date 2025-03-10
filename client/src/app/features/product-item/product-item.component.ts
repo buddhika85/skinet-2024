773cd2c17886dace95475db5d5f686089c5338e2
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-03-10 15:15:14 +1100

    product-item component changes

--- client/src/app/features/product-item/product-item.component.ts
+++ client/src/app/features/product-item/product-item.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../shared/models/Product';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard
+  ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
 export class ProductItemComponent {
 
+  @Input() product?: Product;
 }
